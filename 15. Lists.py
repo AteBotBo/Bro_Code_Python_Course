d6f4416d8c229aacb48ee0cd41a6f66df08e62e9
Nathan Glasby <nathan.glasby@gmail.com>
2023-01-04 20:53:31 +0200

    TEst

--- 15. Lists.py
+++ 15. Lists.py
@@ -1,10 +1,1 @@
 # list = used to store multiple items in a single variable
-
-food = ["pizza","hamburger","hotdog","spaghetti","pudding"]
-
-food.
-
-# print(food[0])
-
-for x in food:
-    print(x)

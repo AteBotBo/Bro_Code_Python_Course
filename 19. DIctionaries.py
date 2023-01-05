8b5159cafe7e6fa0e212e9026fb82e731a13f79b
Nathan Glasby <nathan.glasby@gmail.com>
2023-01-05 14:47:34 +0200

    14:47 - 05/01/23
    Signed-off-by: Nathan Glasby <nathan.glasby@gmail.com>

--- 19. DIctionaries.py
+++ 19. DIctionaries.py
@@ -1,2 +1,21 @@
-# Dictionary = A changable, unordered collection of unique key:value pairs
+# Dictionary = A changeable, unordered collection of unique key:value pairs
 #              Fast because they use hashtag, allow us to access a value quickly.
+
+capitals = {'USA':'Washington DC',
+            'India':'New Dehli',
+            'China':'Beijing',
+            'Russia':'Moscow'}
+
+capitals.update({'Germany':'Berlin'})
+capitals.update({'USA':'Las Vegas'})
+capitals.pop('China')
+capitals.clear()
+
+# print(capitals['Russia'])
+# print(capitals.get('Germany'))
+# print(capitals.keys())
+# print(capitals.values())
+# print(capitals.items())
+
+for key,value in capitals.items():
+    print(key,value)

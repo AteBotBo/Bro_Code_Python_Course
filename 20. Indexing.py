6e6fe00b973ae0058075017a42058b143f6b9e3f
Nathan Glasby <nathan.glasby@gmail.com>
2023-01-05 20:22:32 +0200

    Home - 20:22 - 05/01/23
    Signed-off-by: Nathan Glasby <nathan.glasby@gmail.com>

--- 20. Indexing.py
+++ 20. Indexing.py
@@ -1,15 +1,15 @@
 # index operator [] = gives access tp a sequence's element (str, list, tuples)
 
-name = "nathan Glasby"
+name = "nathan Glasby!"
 
 # if(name[0].islower()):
 #     name = name.capitalize()
 
 first_name = name[:6].upper()
 last_name = name[7:].lower()
+last_character = name[-1]
 
 print(first_name)
 print(last_name)
-
-
+print(last_character)
 

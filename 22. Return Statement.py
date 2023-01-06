a7813d222dee760e79ac358c58cafa7a43441988
Nathan Glasby <nathan.glasby@gmail.com>
2023-01-06 12:06:44 +0200

    Home - 12:09 - 06/01/23
    Signed-off-by: Nathan Glasby <nathan.glasby@gmail.com>

---
+++ 22. Return Statement.py
@@ -0,0 +1,11 @@
+# return statement = Functions send Python values / objects back to the caller.
+#                    These values/objects are known as the function's return value.
+
+def multiply(number1,number2):
+    result = number1 * number2
+    return result
+
+print(multiply(6,8))
+
+print(mul)
+

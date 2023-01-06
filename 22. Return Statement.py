894988a8d56ccd1df1f827de5d94299f9d27cb0b
Nathan Glasby <nathan.glasby@gmail.com>
2023-01-06 15:15:09 +0200

    Home - 15:14 - 06/01/23

--- 22. Return Statement.py
+++ 22. Return Statement.py
@@ -2,10 +2,9 @@
 #                    These values/objects are known as the function's return value.
 
 def multiply(number1,number2):
-    result = number1 * number2
-    return result
+    return number1 * number2
 
-print(multiply(6,8))
+x = multiply(6,8)
 
-print(mul)
+print(x)
 
---
+++ 25. Variable Scope.py
@@ -0,0 +1,8 @@
+name = "Nathan"
+
+def display_name():
+    name = "Glasby"
+    print(name)
+    
+display_name()
+print(name)
